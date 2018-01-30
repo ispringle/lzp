486795aa08a344a7e5a567bebaf55dc7915f0807
pard68 <ipringle@protonmail.com>
2018-01-30 11:12:27 -0500

    added lzp.py

---
+++ lzp.py
@@ -0,0 +1,13 @@
+"""lvp - A loose implementation of Lempel-Ziv compression, for use in determining song complexity/repetition.
+
+This program is based off of a blog post by Colin Morris titled "Are Pop Lyrics Getting More Repetitive?" <https://pudding.cool/2017/05/song-repetition/>. In the blog post Mr. Morris used his own version of the LZ algorithm to determine how repetitive a song is. I have taken my own interpretation of this idea and implemented it in this program.
+
+This program was requested by, and created for, Cliff Stumme aka "The Pop Song Professor". 
+
+Copyright (C) 2018, Ian S. Pringle
+License GNULv3+: GNU GPL Version 3 or later <https://github.com/pard68/lzp/blob/master/LICENSE>
+This is free software: you are free to change and redistribute it.
+There is NO warranty."""
+
+def get_text(file_name):
+	
